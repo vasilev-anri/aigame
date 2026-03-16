3c8cd98dfa5329375bc6d56b345653765fab2f11
vasilevi-anri <vasilev.anri75@gmail.com>
2026-03-16 11:03:40 +0200

    add choosing who starts the game

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,42 @@ void main() {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
 
-    System.out.print("Enter length of number string (15-25): ");
-    int length = scanner.nextInt();
+    boolean playing = true;
 
-    while (length < 15 || length > 25) {
-        System.out.print("Invalid! Enter length between 15 and 25: ");
-        length = scanner.nextInt();
+    while (playing) {
+        System.out.print("Enter length of number string (15-25): ");
+        int length = scanner.nextInt();
+
+        while (length < 15 || length > 25) {
+            System.out.print("Invalid! Enter length between 15 and 25: ");
+            length = scanner.nextInt();
+        }
+
+        List<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < length; i++) {
+            list.add(random.nextInt(9) + 1);
+        }
+
+        System.out.println("Choose who starts the game. 1 - Player, 2 - Computer");
+        int startChoice = scanner.nextInt();
+        while (startChoice != 1 && startChoice != 2) {
+            System.out.println("Invalid! Enter 1 or 2: ");
+            startChoice = scanner.nextInt();
+        }
+
+        boolean playerStarts = (startChoice == 1);
+
+        GameState initialState = new GameState(list, playerStarts);
+        Game game = new Game(initialState);
+        game.play();
+
+        System.out.println("Play again? (y/n): ");
+        String again = scanner.next().toLowerCase();
+        playing = again.equals("y");
+
     }
 
-    List<Integer> list = new LinkedList<>();
-
-    for (int i = 0; i < length; i++) {
-        list.add(random.nextInt(9) + 1);
-    }
-
-//    System.out.println("Generated numbers: " + list);
-
-
-
-
-    GameState initialState = new GameState(list, true);
-
-    Game game = new Game(initialState);
-    game.play();
+    scanner.close();
 
 }
